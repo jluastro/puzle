30921047e40875df2cc62cf2eacdab1783ee1fce
Michael Medford <MichaelMedford@berkeley.edu>
2020-11-16 09:03:25 -0800

    change upload filename to txt

--- code/bin/upload_objects_to_db.sh
+++ code/bin/upload_objects_to_db.sh
@@ -2,15 +2,16 @@
 
 for f in field*objects; do
   echo "Uploading $f";
-  f_db=${f}_db;
-  f_comp=${f_db}.comp;
+  f_db=${f}_db
+  f_txt=${f%objects}txt
+  f_comp=${f_db}.comp
 
   if test -f $f_comp; then
-    echo "--Skipping due to $f_comp";
+    echo "--Skipping due to $f_comp"
     continue
   fi
 
-	sed "s/$/,$f/" $f > ${f_db};
+	sed "s/$/,$f_txt/" $f > ${f_db};
 	psql -U ulens_admin -d ulens -h nerscdb03.nersc.gov -c "\copy object (id, nepochs, filterid, fieldid, rcid, ra, dec, lightcurve_position, lightcurve_filename) FROM '$PWD/$f_db' WITH DELIMITER ',' CSV HEADER;"
 	touch $f_comp
 	rm $f_db
