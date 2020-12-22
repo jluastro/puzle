408fa892d40abd6b6d5b7434c61937e76d5bfba9
Michael Medford <MichaelMedford@berkeley.edu>
2020-12-22 08:08:48 -0500

    working source ingestion

--- bin/populate_source_table.py
+++ bin/populate_source_table.py
@@ -10,7 +10,7 @@ from zort.lightcurveFile import LightcurveFile
 from sqlalchemy.sql.expression import func
 import logging
 
-from puzle.models import Source, SourceIngestJob, Object
+from puzle.models import Source, SourceIngestJob
 from puzle.utils import fetch_job_enddate
 from puzle.ulensdb import insert_db_id, remove_db_id
 from puzle import db
@@ -37,18 +37,19 @@ def convert_obj_to_source(obj, lightcurve_filename):
     }
 
     obj_filt = filter_dict[obj.filterid]
-    source_dict[f'object_id_{obj_filt}'] = obj.objectid
+    source_dict[f'object_id_{obj_filt}'] = obj.object_id
     source_dict[f'lightcurve_position_{obj_filt}'] = obj.lightcurve_position
 
     ra_arr = [obj.ra]
     dec_arr = [obj.dec]
 
-    for sib in obj.siblings:
-        sib_filt = filter_dict[sib.filterid]
-        source_dict[f'object_id_{sib_filt}'] = sib.objectid
-        source_dict[f'lightcurve_position_{sib_filt}'] = sib.lightcurve_position
-        ra_arr.append(sib.ra)
-        dec_arr.append(sib.dec)
+    if obj.siblings:
+        for sib in obj.siblings:
+            sib_filt = filter_dict[sib.filterid]
+            source_dict[f'object_id_{sib_filt}'] = sib.object_id
+            source_dict[f'lightcurve_position_{sib_filt}'] = sib.lightcurve_position
+            ra_arr.append(sib.ra)
+            dec_arr.append(sib.dec)
 
     source_dict['ra'] = np.mean(ra_arr)
     source_dict['dec'] = np.mean(dec_arr)
@@ -102,34 +103,15 @@ def finish_job(job_id):
     remove_db_id()  # release permission for this db connection
 
 
-def upload_sources(source_list, lightcurve_filename):
+def upload_sources(source_list):
     insert_db_id()  # get permission to make a db connection
-    db.session.execute('LOCK TABLE source_ingest_job IN ROW EXCLUSIVE MODE;')
-    _ = db.session.query(SourceIngestJob).with_for_update().\
-        filter(SourceIngestJob.lightcurve_filename == lightcurve_filename).\
-        all()
-    sources_db = db.session.query(Source).\
-        filter(Source.lightcurve_filename == lightcurve_filename).\
-        all()
-    keys_db = set([(s.object_id_g, s.object_id_r, s.object_id_i)
-                   for s in sources_db])
 
     keys_uploading = set()
     for source in source_list:
         key = (source.object_id_g, source.object_id_r, source.object_id_i)
-        if key not in keys_db and key not in keys_uploading:
+        if key not in keys_uploading:
             db.session.add(source)
             keys_uploading.add(key)
-    db.session.commit()
-
-    for key in keys_uploading:
-        for object_id in key:
-            if object_id is None:
-                continue
-            obj = db.session.query(Object).\
-                filter(Object.id == object_id).\
-                first()
-            obj.in_source = True
     db.session.commit()
 
     remove_db_id()  # release permission for this db connection
@@ -172,7 +154,7 @@ def ingest_sources(nepochs_min=20, shutdown_time=5, single_job=False):
 
         num_sources = len(source_list)
         logger.info(f'Job {job_id}: Uploading {num_sources} sources to database')
-        upload_sources(source_list, lightcurve_filename)
+        upload_sources(source_list)
         logger.info(f'Job {job_id}: Upload complete')
 
         finish_job(job_id)
