df92eafd55f4af9a0145b10352fba20af98e1d65
Michael Medford <MichaelMedford@berkeley.edu>
2020-12-22 15:36:56 -0500

    fix tpoy

--- puzle/utils.py
+++ puzle/utils.py
@@ -53,6 +53,6 @@ def lightcurve_file_to_ra_dec(lightcurve_file):
     _, ra_str, dec_str = lightcurve_file.split('_')
     ra0, ra1 = ra_str.replace('ra', '').split('to')
     ra0, ra1 = float(ra0), float(ra1)
-    dec0, dec1 = dec_str.replace('.objects', '').replace('dec', '').split('to')
+    dec0, dec1 = dec_str.replace('.txt', '').replace('dec', '').split('to')
     dec0, dec1 = float(dec0), float(dec1)
     return ra0, ra1, dec0, dec1
