a685d639e496ec0ac5aea8bddac1072c8f6bbdfc
Michael Medford <MichaelMedford@berkeley.edu>
2021-01-07 16:06:44 -0500

    working identify_stars.sh

---
+++ bin/identify_stars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=ulens
+#SBATCH --qos=regular
+#SBATCH --constraint=haswell
+#SBATCH --nodes=1
+#SBATCH --time=42:00:00
+#SBATCH --job-name=stars
+#SBATCH --output=stars.%j.out
+echo "---------------------------"
+echo "Job id = $SLURM_JOBID"
+echo "Proc id = $SLURM_PROCID"
+hostname
+date
+echo "---------------------------"
+
+conda activate puzle
+cd /global/cfs/cdirs/uLens/ZTF/DR3
+srun -N 1 -n 32 python /global/homes/m/mmedford/puzle/bin/identify_stars.py
+
+echo "---------------------------"
+date
+echo "All done!"
+echo "---------------------------"
