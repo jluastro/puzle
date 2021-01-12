0fb498d836c08f788ad60203275c97bb7e5198b4
Michael Medford <MichaelMedford@berkeley.edu>
2021-01-12 11:25:07 -0500

    automate db_con in reset jobs

--- bin/reset_unfinished_ingest_jobs.sh
+++ bin/reset_unfinished_ingest_jobs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-psql -U ulens_admin -d ulens -h nerscdb03.nersc.gov -c "UPDATE source_ingest_job SET started='f', finished='f', datetime_started=NULL, datetime_finished=NULL, slurm_job_id=NULL, slurm_job_rank=NULL WHERE started='t' and finished='f'";
-psql -U ulens_admin -d ulens -h nerscdb03.nersc.gov -c "UPDATE star_ingest_job SET started='f', finished='f', datetime_started=NULL, datetime_finished=NULL, slurm_job_id=NULL, slurm_job_rank=NULL WHERE started='t' and finished='f'";
+if [ -z "$NERSC_HOST" ]
+then
+  db_con="psql ulens -U ulens_admin"
+else
+  db_con="psql -U ulens_admin -d ulens -h nerscdb03.nersc.gov"
+fi
+echo $db_con
+exit 0
+$db_con -c "UPDATE source_ingest_job SET started='f', finished='f', datetime_started=NULL, datetime_finished=NULL, slurm_job_id=NULL, slurm_job_rank=NULL WHERE started='t' and finished='f'";
+$db_con -c "UPDATE star_ingest_job SET started='f', finished='f', datetime_started=NULL, datetime_finished=NULL, slurm_job_id=NULL, slurm_job_rank=NULL WHERE started='t' and finished='f'";
