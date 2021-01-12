c507560b79a1c009bc3e672eec84443574f22593
Michael Medford <MichaelMedford@berkeley.edu>
2021-01-12 12:35:09 -0500

    enforce leading zeros on folder name

--- bin/upload_stars.sh
+++ bin/upload_stars.sh
@@ -9,7 +9,7 @@ fi
 for job_id in $($db_con -c "COPY (SELECT source_ingest_job_id FROM star_ingest_job WHERE finished='t' AND uploaded='f') TO STDOUT");
     do echo "Uploading stars ${job_id}";
     folder=stars_${job_id:0:3};
-    fname=${folder}/stars.${job_id}.txt;
+    fname=${folder}/stars.$(printf %06d $job_id).txt;
     $db_con -c "\copy star (ra,dec,ingest_job_id,source_ids) from ${fname} delimiter ',' csv header NULL as 'None'";
     $db_con -c "update star_ingest_job set uploaded='t' where source_ingest_job_id=${job_id}";
 done
