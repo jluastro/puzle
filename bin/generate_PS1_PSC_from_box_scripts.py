d89ba0c6b45ed40ec95a63c36dd14357ba409f86
Michael Medford <MichaelMedford@berkeley.edu>
2021-03-01 11:57:00 -0500

    pass overwrite flag

--- bin/generate_PS1_PSC_from_box_scripts.py
+++ bin/generate_PS1_PSC_from_box_scripts.py
@@ -38,7 +38,12 @@ def main():
     parser.add_argument('--n-scripts', type=int,
                           help='Number of scripts to split download into.',
                           default=1)
+    parser.add_argument('--overwrite', dest='overwrite', action='store_true')
+    parser.set_defaults(overwrite=False)
+
     args = parser.parse_args()
+    n_scripts = args.n_scripts
+    overwrite = args.overwrite
 
     data_dir = return_data_dir()
     config_fname = '%s/800207941_6guv3vhr_config.json' % data_dir
@@ -48,25 +53,27 @@ def main():
     _ = client.create_user('User', login=None)
     access_token = config.access_token
 
-    template = 'curl https://api.box.com/2.0/files/{file_id}/content ' \
-               '-H "Authorization: Bearer {access_token}" ' \
-               '-H "BoxApi: shared_link=https://northwestern.app.box.com/s/' \
-               '94tv3ry0tlz6tbfmv7aznj5wzzneoqpn" -L --output {file_name}'
-
     url = "https://northwestern.app.box.com/s/94tv3ry0tlz6tbfmv7aznj5wzzneoqpn"
     shared_folder = client.get_shared_item(url)
 
-    init_download_scripts(args.n_scripts)
+    template = 'curl https://api.box.com/2.0/files/{file_id}/content ' \
+               '-H "Authorization: Bearer {access_token}" ' \
+               '-H "BoxApi: shared_link={url}" -L --output {file_name}'
+
+    init_download_scripts(n_scripts)
 
     for i, file in enumerate(shared_folder.get_items()):
         if file.type == 'file':
+            if overwrite and os.path.exists(file.name):
+                continue
             dict_out = {'access_token': access_token,
-                        'file_id': file.id, 'file_name':
-                            file.name}
+                        'url': url,
+                        'file_id': file.id,
+                        'file_name': file.name}
             cmd = template.format(**dict_out)
-            output_cmd(cmd, file.name, i, args.n_scripts)
+            output_cmd(cmd, file.name, i, n_scripts)
 
-    open_script_permissions(args.n_scripts)
+    open_script_permissions(n_scripts)
 
 
 if __name__ == '__main__':
