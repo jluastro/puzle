308503496bcf11419d2adba62b76c4b3c977d47b
Michael Medford <MichaelMedford@berkeley.edu>
2021-03-01 08:59:36 -0800

    init export_sources.sh

---
+++ bin/export_sources.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=m2218
+#SBATCH --qos=premium
+#SBATCH --constraint=haswell
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=export-sources
+#SBATCH --output=export-sources.%j.out
+echo "---------------------------"
+echo "Job id = $SLURM_JOBID"
+echo "Proc id = $SLURM_PROCID"
+hostname
+date
+echo "---------------------------"
+
+conda activate puzle
+cd /global/cfs/cdirs/uLens/ZTF/DR3
+srun -N 1 -n 32 python export_sources.py
+
+echo "---------------------------"
+date
+echo "All done!"
+echo "---------------------------"
