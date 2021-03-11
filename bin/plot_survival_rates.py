3123d91903f0ddb34fe359cef6ec7fe52f205f48
Michael Medford <MichaelMedford@berkeley.edu>
2021-03-11 10:59:10 -0500

    plot low and high jobs

--- bin/plot_survival_rates.py
+++ bin/plot_survival_rates.py
@@ -9,59 +9,91 @@ from puzle import db
 from puzle.models import StarProcessJob, SourceIngestJob
 
 
-def parse_cut_rates(job):
+def parse_surv_rates(job):
     num_objs = job.num_objs
     num_objs_pass_eta = job.num_objs_pass_eta
     num_objs_pass_eta_residual = job.num_objs_pass_eta_residual
     num_objs_pass_rf = job.num_objs_pass_rf
 
-    eta_cut_rate = np.log10(num_objs_pass_eta / num_objs)
-    rf_cut_rate = np.log10(num_objs_pass_rf / num_objs_pass_eta)
-    eta_residual_rate = np.log10(num_objs_pass_eta_residual / num_objs_pass_rf)
+    eta_surv_rate = np.log10(num_objs_pass_eta / num_objs)
+    if eta_surv_rate == 0:
+        rf_surv_rate = 0
+        eta_residual_rate = 0
+    else:
+        rf_surv_rate = np.log10(num_objs_pass_rf / num_objs_pass_eta)
+        if rf_surv_rate == 0:
+            eta_residual_rate = 0
+        else:
+            eta_residual_rate = np.log10(num_objs_pass_eta_residual / num_objs_pass_rf)
 
-    return eta_cut_rate, rf_cut_rate, eta_residual_rate
+    return eta_surv_rate, rf_surv_rate, eta_residual_rate
 
 
 def plot_survival_rates():
-    jobs = db.session.query(StarProcessJob).filter(StarProcessJob.finished==True).all()
-    eta_cur_rate_arr = []
-    rf_cut_rate_arr = []
-    eta_residual_rate_arr = []
-    for job in jobs:
-        eta_cut_rate, rf_cut_rate, eta_residual_rate = parse_cut_rates(job)
-        eta_cur_rate_arr.append(eta_cut_rate)
-        rf_cut_rate_arr.append(rf_cut_rate)
-        eta_residual_rate_arr.append(eta_residual_rate)
+    jobs_low = db.session.query(StarProcessJob).filter(StarProcessJob.finished==True,
+                                                       StarProcessJob.num_stars!=0,
+                                                       StarProcessJob.priority>10000).all()
+    num_jobs_low = len(jobs_low)
+    eta_cur_rate_arr_low = []
+    rf_surv_rate_arr_low = []
+    eta_residual_rate_arr_low = []
+    for job in jobs_low:
+        eta_surv_rate, rf_surv_rate, eta_residual_rate = parse_surv_rates(job)
+        eta_cur_rate_arr_low.append(eta_surv_rate)
+        rf_surv_rate_arr_low.append(rf_surv_rate)
+        eta_residual_rate_arr_low.append(eta_residual_rate)
+        
+    jobs_high = db.session.query(StarProcessJob).filter(StarProcessJob.finished==True,
+                                                       StarProcessJob.num_stars!=0,
+                                                       StarProcessJob.priority<10000).all()
+    num_jobs_high = len(jobs_high)
+    eta_cur_rate_arr_high = []
+    rf_surv_rate_arr_high = []
+    eta_residual_rate_arr_high = []
+    for job in jobs_high:
+        eta_surv_rate, rf_surv_rate, eta_residual_rate = parse_surv_rates(job)
+        eta_cur_rate_arr_high.append(eta_surv_rate)
+        rf_surv_rate_arr_high.append(rf_surv_rate)
+        eta_residual_rate_arr_high.append(eta_residual_rate)
 
     fig, ax = plt.subplots(1, 3, figsize=(12, 5))
     for a in ax:
         a.clear()
     ax[0].set_title('Eta Cut', fontsize=12)
     ax[0].set_xlabel('log(survival rate)', fontsize=12)
-    ax[0].hist(eta_cur_rate_arr, bins=6, histtype='step', label='puzle')
-    ax[0].axvline(np.log10(0.005), color='r', alpha=.4, label='Price-Whelan')
-    ax[0].legend(loc=9)
+    ax[0].hist(eta_cur_rate_arr_low, bins=6, histtype='step',
+               label='puzle low', color='b')
+    ax[0].hist(eta_cur_rate_arr_high, bins=6, histtype='step',
+               label='puzle high', color='g')
+    ax[0].axvline(np.log10(0.01), color='r', alpha=.4, label='Price-Whelan')
+    ax[0].legend(loc=1)
     ax[1].set_title('Star/Galaxy Cut', fontsize=12)
     ax[1].set_xlabel('log(survival rate)', fontsize=12)
-    ax[1].hist(rf_cut_rate_arr, bins=6, histtype='step', label='puzle')
+    ax[1].hist(rf_surv_rate_arr_low, bins=6, histtype='step',
+               label='puzle low', color='b')
+    ax[1].hist(rf_surv_rate_arr_high, bins=6, histtype='step',
+               label='puzle high', color='g')
     ax[1].axvline(np.log10(0.10), color='r', alpha=.4, label='Price-Whelan')
-    ax[1].legend(loc=9)
+    ax[1].legend(loc=1)
     ax[2].set_title('Eta Residual Cut', fontsize=12)
     ax[2].set_xlabel('log(survival rate)', fontsize=12)
-    ax[2].hist(eta_residual_rate_arr, bins=6, histtype='step', label='puzle')
+    ax[2].hist(eta_residual_rate_arr_low, bins=6, histtype='step',
+               label='puzle low', color='b')
+    ax[2].hist(eta_residual_rate_arr_high, bins=6, histtype='step',
+               label='puzle high', color='g')
     ax[2].axvline(np.log10(0.10), color='r', alpha=.4, label='Price-Whelan')
-    ax[2].legend(loc=9)
+    ax[2].legend(loc=1)
     fig.tight_layout()
-    fig.subplots_adjust(top=.9)
-    fig.suptitle('25 Sample Fields', fontsize=10)
+    fig.subplots_adjust(top=.85)
+    fig.suptitle('%i Low Jobs | %i High Jobs' % (num_jobs_low, num_jobs_high))
 
 
 def plot_job_locations():
-    # grab ra_arr, dec_arr, N_sources_arr from plot_ingest_progress
+    # grab ra_arr_low, dec_arr_low, N_sources_arr_low from plot_ingest_progress
     fig, ax = plt.subplots(figsize=(12, 5))
-    im = ax.scatter(ra_arr, dec_arr, c=N_sources_arr, edgecolor='None', s=5)
-    cond = N_sources_arr == 0
-    ax.scatter(ra_arr[cond], dec_arr[cond], c='r', edgecolor='None', s=5)
+    im = ax.scatter(ra_arr_low, dec_arr_low, c=N_sources_arr_low, edgecolor='None', s=5)
+    cond = N_sources_arr_low == 0
+    ax.scatter(ra_arr_low[cond], dec_arr_low[cond], c='r', edgecolor='None', s=5)
     cbar = fig.colorbar(im, ax=ax)
     cbar.set_label('log(num sources)', fontsize=12)
     ax.set_xlabel('ra', fontsize=12)
@@ -73,14 +105,14 @@ def plot_job_locations():
         filter(StarProcessJob.finished==True).all()
 
     # find the density at the center of the job field
-    ra_job_arr = []
-    dec_job_arr = []
+    ra_job_arr_low = []
+    dec_job_arr_low = []
     for source_ingest_job, _ in jobs:
         ra = (source_ingest_job.ra_start + source_ingest_job.ra_end) / 2.
         dec = (source_ingest_job.dec_start + source_ingest_job.dec_end) / 2.
-        ra_job_arr.append(ra)
-        dec_job_arr.append(dec)
-    ax.scatter(ra_job_arr, dec_job_arr, marker='*', s=5, color='k')
+        ra_job_arr_low.append(ra)
+        dec_job_arr_low.append(dec)
+    ax.scatter(ra_job_arr_low, dec_job_arr_low, marker='*', s=5, color='k')
 
 
 
