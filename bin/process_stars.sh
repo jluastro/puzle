cece0a80c39af05eb9647a76388884b897bc6cc6
Michael Medford <MichaelMedford@berkeley.edu>
2021-03-22 14:59:19 -0400

    create process_stars script

---
+++ bin/process_stars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=m2218
+#SBATCH --qos=regular
+#SBATCH --constraint=haswell
+#SBATCH --nodes=1
+#SBATCH --time=07:00:00
+#SBATCH --job-name=stars
+#SBATCH --output=stars.%j.out
+echo "---------------------------"
+echo "Job id = $SLURM_JOBID"
+echo "Proc id = $SLURM_PROCID"
+hostname
+date
+echo "---------------------------"
+
+conda activate puzle
+cd /global/cfs/cdirs/uLens/ZTF/DR4
+srun -N 1 -n 32 python /global/homes/m/mmedford/puzle/bin/process_stars.py
+
+echo "---------------------------"
+date
+echo "All done!"
+echo "---------------------------"
