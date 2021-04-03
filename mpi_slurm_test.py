910af768b846becd067c4752c61d7f8ae1b013cf
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-03 09:00:00 -0400

    move logger into puzle.utils

--- mpi_slurm_test.py
+++ mpi_slurm_test.py
@@ -1,20 +1,13 @@
-import os
-from puzle.utils import execute
-import logging
+from puzle.utils import execute, get_logger
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
 size = comm.size
 
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger = get_logger(__name__)
 
 logger.info('Hello World %i of %i' % (rank, size))
-
-# os.environ['PATH'] += os.pathsep + '/global/common/shared/das/container_proxy'
 
 if rank == 0:
     stdout, _ = execute('squeue --noheader -u mmedford --format="%i')
