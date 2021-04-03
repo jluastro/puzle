d3262b964002ecc8026a5bc9f7fcd43cea785915
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-03 08:26:53 -0400

    test

--- mpi_slurm_test.py
+++ mpi_slurm_test.py
@@ -10,6 +10,6 @@ print('Hello World %i of %i' % (rank, size))
 
 os.environ['PATH'] += os.pathsep + '/global/common/shared/das/container_proxy'
 
-stdout, _ = execute('squeue --noheader -u mmedford --format="%i')
 if rank == 0:
+    stdout, _ = execute('squeue --noheader -u mmedford --format="%i')
     print(stdout)
