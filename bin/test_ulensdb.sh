4ee02e3e35ed2267becea8212c6eddb43810e655
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-05 10:55:03 -0400

    v0.1.13

--- bin/test_ulensdb.sh
+++ bin/test_ulensdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=m2218
-#SBATCH --image=registry.services.nersc.gov/mmedford/puzle:v0.1.12
+#SBATCH --image=registry.services.nersc.gov/mmedford/puzle:v0.1.13
 #SBATCH --qos=debug
 #SBATCH --constraint=haswell
 #SBATCH --nodes=10
