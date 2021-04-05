b15ef5b423f4f7c4b473c02d15c9fa105a081faa
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-05 11:21:19 -0400

    fix typo

--- bin/process_stars.sh
+++ bin/process_stars.sh
@@ -5,8 +5,8 @@
 #SBATCH --constraint=haswell
 #SBATCH --nodes=10
 #SBATCH --time=00:30:00
-#SBATCH --job-name=test
-#SBATCH --output=test.%j.out
+#SBATCH --job-name=stars
+#SBATCH --output=stars.%j.out
 echo "---------------------------"
 echo "Job id = $SLURM_JOBID"
 echo "Proc id = $SLURM_PROCID"
@@ -14,6 +14,7 @@ hostname
 date
 echo "---------------------------"
 
+fname=/global/u2/m/mmedford/puzle/data/ulensdb/current_slurm_job_ids.txt
 echo $SLURM_JOBID >> $fname
 srun -N 10 -n 320 shifter --volume="/global/cfs/cdirs/uLens/ZTF/DR4:/home/puzle/data/DR4;/global/cfs/cdirs/uLens/PS1_PSC:/home/puzle/data/PS1_PSC;/global/u2/m/mmedford/puzle/data/ulensdb:/home/puzle/data/ulensdb" python /home/puzle/test_ulensdb.py
 sed -i "/${SLURM_JOBID}/d" "$fname"
