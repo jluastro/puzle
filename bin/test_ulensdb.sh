963ff9f0fed28c3440daed7960ea0f8d00429ef1
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-05 10:06:37 -0400

    320 cores on 10 nodes

--- bin/test_ulensdb.sh
+++ bin/test_ulensdb.sh
@@ -3,7 +3,7 @@
 #SBATCH --image=registry.services.nersc.gov/mmedford/puzle:v0.1.9
 #SBATCH --qos=debug
 #SBATCH --constraint=haswell
-#SBATCH --nodes=1
+#SBATCH --nodes=10
 #SBATCH --time=00:30:00
 #SBATCH --job-name=test
 #SBATCH --output=test.%j.out
@@ -18,7 +18,7 @@ export PROXY_SOCKET=/tmp/${USER}.${SLURM_JOB_ID}.sock
 /global/common/shared/das/container_proxy/server.py &
 CPID=$!
 
-srun -N 1 -n 32 shifter --volume="/global/cfs/cdirs/uLens/ZTF/DR4:/home/puzle/data/DR4;/global/cfs/cdirs/uLens/PS1_PSC:/home/puzle/data/PS1_PSC;/global/u2/m/mmedford/puzle/data/ulensdb:/home/puzle/data/ulensdb" python /home/puzle/test_ulensdb.py
+srun -N 10 -n 320 shifter --volume="/global/cfs/cdirs/uLens/ZTF/DR4:/home/puzle/data/DR4;/global/cfs/cdirs/uLens/PS1_PSC:/home/puzle/data/PS1_PSC;/global/u2/m/mmedford/puzle/data/ulensdb:/home/puzle/data/ulensdb" python /home/puzle/test_ulensdb.py
 
 kill $CPID
 
