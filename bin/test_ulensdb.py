636f2fb0132e9e9c51e1f95147a01e4ae9391018
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-05 09:25:42 -0400

    set random seed to rank

--- bin/test_ulensdb.py
+++ bin/test_ulensdb.py
@@ -6,8 +6,18 @@ test_ulensdb.py
 import numpy as np
 import time
 from puzle.ulensdb import insert_db_id, remove_db_id
+from puzle.utils import get_logger
 
+logger = get_logger(__name__)
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+logger.info(f'{rank}: Before sleep')
+np.random.seed(rank)
 time.sleep(abs(np.random.normal()))
+logger.info(f'{rank}: After sleep')
 
 insert_db_id()
 time.sleep(10)
