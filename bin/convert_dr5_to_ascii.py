8d84b34bb0d7b0baf3bb49284c01a3977602be64
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-07 17:24:08 -0400

    remove to_pandas

--- bin/convert_dr5_to_ascii.py
+++ bin/convert_dr5_to_ascii.py
@@ -6,7 +6,7 @@ convert_dr5_to_ascii.py
 import glob
 import os
 import numpy as np
-import pyarrow.parquet as pq
+import pandas as pd
 
 
 def sort_files_into_folders(path='/global/cfs/cdirs/uLens/ZTF/DR5'):
@@ -24,7 +24,7 @@ def sort_files_into_folders(path='/global/cfs/cdirs/uLens/ZTF/DR5'):
         os.rename(file, file_new)
 
 
-def parquet_to_asciilc(inpath, outdir='/global/cfs/cdirs/uLens/ZTF/DR5'):
+def parquet_to_ascii(inpath, outdir='/global/cfs/cdirs/uLens/ZTF/DR5'):
     """
     Convert ZTF lightcurve Parquet dataset for one field to the pre-DR5 ascii format
 
@@ -38,34 +38,34 @@ def parquet_to_asciilc(inpath, outdir='/global/cfs/cdirs/uLens/ZTF/DR5'):
         path to directory for output of ascii text file for zort
 
     """
-    ds = pq.ParquetDataset(inpath, use_legacy_dataset=False)
-    df = ds.read().to_pandas()
-    df.sort_values(['filterid', 'rcid'],
-                   ascending=[False, True],
-                   inplace=True)
-    fields = df.fieldid.unique()
-    assert len(fields) == 1
-    field = fields[0]
-    ramin = df.objra.min()
-    ramax = df.objra.max()
-    decmin = df.objdec.min()
-    decmax = df.objdec.max()
-    outname = os.path.join(outdir,
-                           'field{:06d}_ra{:.5f}to{:.5f}_dec{:.5f}to{:.5f}.txt'.format(
-                               field, ramin, ramax, decmin, decmax))
-    with open(outname, 'w') as fh:
-        for row in df.itertuples():
-            fh.write("# %17d %3d %1d %4d %2d %9.5f %9.5f\n" %
-                     (row.objectid, row.nepochs, row.filterid, row.fieldid,
-                      row.rcid, row.objra, row.objdec))
-            idx = np.argsort(row.hmjd)
-            for i in idx:
-                fh.write("  %13.5f %6.3f %5.3f %6.3f %5d\n" %
-                         (row.hmjd[i], row.mag[i], row.magerr[i],
-                          row.clrcoeff[i], row.catflags[i]))
+    files = glob.glob(f'{inpath}/*parquet')
+    field = int(inpath.split('/')[-1].replace('field', ''))
+    fname_tmp = f'{outdir}/field{field:06d}_tmp.txt'
+    ra_arr = []
+    dec_arr = []
+    with open(fname_tmp, 'w') as fh:
+        for f in files:
+            df = pd.read_parquet(f, engine='pyarrow')
+            for row in df.itertuples():
+                fh.write("# %17d %3d %1d %4d %2d %9.5f %9.5f\n" %
+                         (row.objectid, row.nepochs, row.filterid, row.fieldid,
+                          row.rcid, row.objra, row.objdec))
+                ra_arr.append(row.objra)
+                dec_arr.append(row.objdec)
+                idx = np.argsort(row.hmjd)
+                for i in idx:
+                    fh.write("  %13.5f %6.3f %5.3f %6.3f %5d\n" %
+                             (row.hmjd[i], row.mag[i], row.magerr[i],
+                              row.clrcoeff[i], row.catflags[i]))
 
-    del df, ds
-    return
+    ramin = np.min(ra_arr)
+    ramax = np.max(ra_arr)
+    decmin = np.min(dec_arr)
+    decmax = np.max(dec_arr)
+    fname = os.path.join(outdir,
+                         'field{:06d}_ra{:.5f}to{:.5f}_dec{:.5f}to{:.5f}.txt'.format(
+                             field, ramin, ramax, decmin, decmax))
+    os.rename(fname_tmp, fname)
 
 
 def return_unconverted_folders(path):
@@ -96,8 +96,8 @@ def convert_DR5_to_ascii(path='/global/cfs/cdirs/uLens/ZTF/DR5'):
 
     for i, folder in enumerate(my_folders):
         print('%i) Converting %s (%i / %i)' % (rank, folder, i, len(my_folders)))
-        parquet_to_asciilc(inpath=folder,
-                           outdir=path)
+        parquet_to_ascii(inpath=folder,
+                         outdir=path)
 
 
 if __name__ == '__main__':
