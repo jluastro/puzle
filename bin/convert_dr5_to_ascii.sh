4ad5dc48b14e85fba868767f2d155e94a8acdf6c
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-06 22:11:54 -0400

    convert_dr5_to_ascii.sh

---
+++ bin/convert_dr5_to_ascii.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=m2218
+#SBATCH --qos=premium
+#SBATCH --constraint=haswell
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=convert
+#SBATCH --output=convert.%j.out
+echo "---------------------------"
+echo "Job id = $SLURM_JOBID"
+echo "Proc id = $SLURM_PROCID"
+hostname
+date
+echo "---------------------------"
+
+conda activate puzle
+srun -N 1 -n 32 python /global/homes/m/mmedford/puzle/bin/convert_dr5_to_ascii.py
+
+echo "---------------------------"
+date
+echo "All done!"
+echo "---------------------------"
