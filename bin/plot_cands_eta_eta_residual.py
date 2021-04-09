7fc22347f2e3e28c6598ef5dd95dd8de889fe692
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-09 08:37:14 -0400

    plot_ulens_eta_by plots

--- bin/plot_cands_eta_eta_residual.py
+++ bin/plot_cands_eta_eta_residual.py
@@ -374,6 +374,108 @@ def plot_ulens_tE_piE_vs_eta(eta_ulens_arr, eta_residual_ulens_arr, observable_a
     plt.close(fig)
 
 
+def plot_ulens_eta_by_mag(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr):
+    fname = '%s/ulens_sample_metadata.total.npz' % return_data_dir()
+    metadata = np.load(fname)
+    mag_src = metadata['mag_src']
+
+    cond_obs = observable_arr == True
+
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    ax[0].set_title('All Observable mag_src')
+    ax[0].hexbin(eta_ulens_arr[cond_obs], eta_residual_ulens_arr[cond_obs],
+                 mincnt=1, gridsize=25)
+    mag_src_bins = [0, 17.5, 18.5, 19.5, 20.5, 21.5]
+    for i in range(5):
+        mag_src_low = mag_src_bins[i]
+        mag_src_high = mag_src_bins[i+1]
+        cond_mag_src = (mag_src > mag_src_low) * (mag_src <= mag_src_high)
+        ax[i+1].set_title('%.1f < mag_src <= %.1f' % (mag_src_low, mag_src_high))
+        ax[i+1].hexbin(eta_ulens_arr[cond_obs*cond_mag_src],
+                       eta_residual_ulens_arr[cond_obs*cond_mag_src],
+                       mincnt=1, gridsize=25)
+
+        ax[i+1].grid(True)
+        ax[i+1].set_xlabel('eta', fontsize=10)
+        ax[i+1].set_ylabel('eta_residual', fontsize=10)
+    fig.tight_layout()
+
+    figures_dir = return_figures_dir()
+    fname = f'{figures_dir}/ulens_eta_by_mag.png'
+    fig.savefig(fname, dpi=100, bbox_inches='tight', pad_inches=0.01)
+    print('-- %s saved' % fname)
+
+
+def plot_ulens_eta_by_tE(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr):
+    fname = '%s/ulens_sample_metadata.total.npz' % return_data_dir()
+    metadata = np.load(fname)
+    tE = metadata['tE']
+
+    cond_obs = observable_arr == True
+
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    ax[0].set_title('All Observable tE')
+    ax[0].hexbin(eta_ulens_arr[cond_obs], eta_residual_ulens_arr[cond_obs],
+                 mincnt=1, gridsize=25)
+    tE_bins = [20, 50, 80, 120, 150, 1000]
+    for i in range(5):
+        tE_low = tE_bins[i]
+        tE_high = tE_bins[i+1]
+        cond_tE = (tE > tE_low) * (tE <= tE_high)
+        ax[i+1].set_title('%.1f < tE <= %.1f' % (tE_low, tE_high))
+        ax[i+1].hexbin(eta_ulens_arr[cond_obs*cond_tE],
+                       eta_residual_ulens_arr[cond_obs*cond_tE],
+                       mincnt=1, gridsize=25)
+
+        ax[i+1].grid(True)
+        ax[i+1].set_xlabel('eta', fontsize=10)
+        ax[i+1].set_ylabel('eta_residual', fontsize=10)
+    fig.tight_layout()
+
+    figures_dir = return_figures_dir()
+    fname = f'{figures_dir}/ulens_eta_by_tE.png'
+    fig.savefig(fname, dpi=100, bbox_inches='tight', pad_inches=0.01)
+    print('-- %s saved' % fname)
+
+
+def plot_ulens_eta_by_piE(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr):
+    fname = '%s/ulens_sample_metadata.total.npz' % return_data_dir()
+    metadata = np.load(fname)
+    piE = np.hypot(metadata['piE_E'], metadata['piE_N'])
+
+    cond_obs = observable_arr == True
+
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    ax[0].set_title('All Observable piE')
+    ax[0].hexbin(eta_ulens_arr[cond_obs], eta_residual_ulens_arr[cond_obs],
+                 mincnt=1, gridsize=25)
+    piE_bins = np.logspace(-2, np.log10(3), 6)
+    for i in range(5):
+        piE_low = piE_bins[i]
+        piE_high = piE_bins[i+1]
+        cond_piE = (piE > piE_low) * (piE <= piE_high)
+        ax[i+1].set_title('%.2f < piE <= %.2f' % (piE_low, piE_high))
+        ax[i+1].hexbin(eta_ulens_arr[cond_obs*cond_piE],
+                       eta_residual_ulens_arr[cond_obs*cond_piE],
+                       mincnt=1, gridsize=25)
+
+        ax[i+1].grid(True)
+        ax[i+1].set_xlabel('eta', fontsize=10)
+        ax[i+1].set_ylabel('eta_residual', fontsize=10)
+    fig.tight_layout()
+
+    figures_dir = return_figures_dir()
+    fname = f'{figures_dir}/ulens_eta_by_piE.png'
+    fig.savefig(fname, dpi=100, bbox_inches='tight', pad_inches=0.01)
+    print('-- %s saved' % fname)
+    
+
 def generate_all_plots():
     eta_arr, eta_residual_arr = return_eta_arrs()
     eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr = return_eta_ulens_arrs()
@@ -384,6 +486,9 @@ def generate_all_plots():
     plot_lowest_ulens_eta(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr)
     plot_ulens_tE_piE(observable_arr)
     plot_ulens_tE_piE_vs_eta(eta_ulens_arr, eta_residual_ulens_arr, observable_arr)
+    plot_ulens_eta_by_mag(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr)
+    plot_ulens_eta_by_tE(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr)
+    plot_ulens_eta_by_piE(eta_ulens_arr, eta_residual_ulens_arr, eta_residual_actual_ulens_arr, observable_arr)
 
 
 if __name__ == '__main__':
