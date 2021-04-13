7b09271141a32c6606744513797c4dc0021fdc45
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-13 14:43:28 -0400

    enforce no nan in fit_event

--- puzle/fit.py
+++ puzle/fit.py
@@ -63,6 +63,13 @@ def ulens_func_a_type_two(t_arr, t0, t_eff, f0, f1):
 
 def fit_event(t_obs_arr, mag_arr, magerr_arr):
     flux_obs_arr, fluxerr_obs_arr = magnitudes_to_fluxes(mag_arr, magerr_arr)
+    cond = ~np.isnan(t_obs_arr)
+    cond *= ~np.isnan(flux_obs_arr)
+    cond *= ~np.isnan(fluxerr_obs_arr)
+    t_obs_arr = t_obs_arr[cond]
+    flux_obs_arr = flux_obs_arr[cond]
+    fluxerr_obs_arr = fluxerr_obs_arr[cond]
+
     bounds = ([np.min(t_obs_arr) - 50, 0.01, -np.inf, 0],
               [np.max(t_obs_arr) + 50, 5000, np.inf, np.inf])
 
