75568033ad069f51c25d20b053aa16968b1fe724
Michael Medford <MichaelMedford@berkeley.edu>
2021-05-07 19:20:14 -0400

    add wait command

--- bin/pipeline/pspl_gp/fit_level4_candidates_to_pspl_gp.sh
+++ bin/pipeline/pspl_gp/fit_level4_candidates_to_pspl_gp.sh
@@ -13,7 +13,6 @@ hostname
 date
 echo "---------------------------"
 
-cd /global/homes/m/mmedford/puzle/bin/pipeline/pspl_gp
 module load cmake/3.18.2
 module unload craype-hugepages2M
 export LD_LIBRARY_PATH=/global/cfs/cdirs/uLens/code/src/MultiNest/lib:$LD_LIBRARY_PATH
@@ -23,9 +22,10 @@ nodelist=$(python /global/homes/m/mmedford/puzle/bin/pipeline/parse_nersc_nodeli
 echo "Nodes = $nodelist"
 
 for node_name in $nodelist; do
-  fname_log="pspl_gp.$SLURM_JOBID.$node_name.log"
+  fname_log="/global/homes/m/mmedford/puzle/bin/pipeline/pspl_gp/pspl_gp.$SLURM_JOBID.$node_name.log"
   srun -N 1 -n 32 -w $node_name python /global/homes/m/mmedford/puzle/bin/pipeline/fit_level4_candidates_to_pspl_gp.py $SLURM_JOBID $node_name > $fname_log &
-done
+done;
+wait
 
 echo "---------------------------"
 date
