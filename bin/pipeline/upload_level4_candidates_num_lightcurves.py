b541d823370314440c99a297298dd80fc59969d4
Michael Medford <MichaelMedford@berkeley.edu>
2021-05-11 08:07:38 -0400

    fix typo

--- bin/pipeline/upload_level4_candidates_num_lightcurves.py
+++ bin/pipeline/upload_level4_candidates_num_lightcurves.py
@@ -13,7 +13,6 @@ from puzle import db
 
 def upload_level4_candidates_num_lightcurves():
     cands = CandidateLevel4.query.order_by(CandidateLevel4.id).all()
-    num_cands = len(cands)
 
     if 'SLURMD_NODENAME' in os.environ:
         from mpi4py import MPI
@@ -25,7 +24,8 @@ def upload_level4_candidates_num_lightcurves():
         size = 1
 
     my_cands = np.array_split(cands, size)[rank]
-    print('Rank %i) %i candidates to upload num_lightcurves' % (rank, num_cands))
+    my_num_cands = len(my_cands)
+    print('Rank %i) %i candidates to upload num_lightcurves' % (rank, my_num_cands))
 
     for cand in my_cands:
         cand_fitter_data = load_cand_fitter_data(cand.id)
