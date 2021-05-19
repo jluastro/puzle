be3dec7fb3186ecc95df708d2ae1d1b2857270c4
Michael Medford <MichaelMedford@berkeley.edu>
2021-05-18 20:04:26 -0400

    new version of level5 cuts

--- bin/paper_figures/plot_pspl_gp_fit_cuts.py
+++ bin/paper_figures/plot_pspl_gp_fit_cuts.py
@@ -6,39 +6,129 @@ plot_pspl_gp_fit_cuts.py
 import matplotlib.pyplot as plt
 import numpy as np
 
-from puzle.cands import apply_level4_cuts_to_query
 from puzle.models import CandidateLevel4
-from puzle.utils import return_figures_dir
 
 
 def plot_pspl_gp_fit_cuts():
     cands = CandidateLevel4.query.\
         filter(CandidateLevel4.pspl_gp_fit_finished == True,
                CandidateLevel4.fit_type_pspl_gp != None).\
-        with_entities(CandidateLevel4.piE_pspl_gp).\
         all()
 
-    piE_cands = np.array([c[0] for c in cands])
+    keys = ['t0',
+            'u0_amp',
+            'u0_amp_err',
+            'tE',
+            'tE_err',
+            'b_sff',
+            'b_sff_err',
+            'piE_E',
+            'piE_E_err',
+            'piE_N',
+            'piE_N_err',
+            'piE',
+            'piE_err',
+            'rchi2']
+    keys_err = [k for k in keys if f'{k}_err' in keys]
 
-    query = apply_level4_cuts_to_query(CandidateLevel4.query)
-    cands_cut = query.\
-        filter(CandidateLevel4.pspl_gp_fit_finished == True,
-               CandidateLevel4.fit_type_pspl_gp != None).\
-        with_entities(CandidateLevel4.piE_pspl_gp).\
-        all()
+    data = {}
+    for key in keys:
+        data[key] = np.array([getattr(c, f'{key}_pspl_gp') for c in cands])
+    data['cand_id'] = np.array([c.id for c in cands])
 
-    piE_cands_cut = np.array([c[0] for c in cands_cut])
+    error_frac = {}
+    for key in keys_err:
+        key_err = f'{key}_err'
+        error_frac[key] = data[key_err] / data[key]
 
-    fig, ax = plt.subplots(2, 2, figsize=(10, 5))
+    error = {}
+    for key in keys_err:
+        key_err = f'{key}_err'
+        error[key_err] = data[key_err]
+
+    cond1 = error_frac['tE'] <= 0.2
+    cond2 = data['u0_amp_err'] <= 0.1
+    cond3 = data['piE_err'] <= 0.1
+    cond4 = np.abs(data['u0_amp']) <= 1.0
+    cond5 = data['b_sff'] <= 1.2
+    cond6 = data['rchi2'] <= 3
+
+    level5_cond = cond1 * cond2 * cond3 * cond4 * cond5 * cond6
+
+    print('No filters', len(cond1), 'cands')
+    print('Filters up to 1', np.sum(cond1), 'cands')
+    print('Filters up to 2', np.sum(cond1 * cond2), 'cands')
+    print('Filters up to 3', np.sum(cond1 * cond2 * cond3), 'cands')
+    print('Filters up to 4', np.sum(cond1 * cond2 * cond3 * cond4), 'cands')
+    print('Filters up to 5', np.sum(cond1 * cond2 * cond3 * cond4 * cond5), 'cands')
+    print('Filters up to 6', np.sum(cond1 * cond2 * cond3 * cond4 * cond5 * cond6), 'cands')
+
+    # fractional error hist
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
     ax = ax.flatten()
     for a in ax: a.clear()
-
+    for i, key in enumerate(keys_err):
+        arr = error_frac[key]
+        bins = np.linspace(-2, 2, 50)
+        ax[i].hist(arr, bins=bins, histtype='step', density=True)
+        ax[i].hist(arr[level5_cond], bins=bins, histtype='step', density=True)
+        ax[i].set_xlabel(f'fractional error: {key}')
     fig.tight_layout()
 
-    fname = '%s/plot_pspl_gp_fit_cuts.png' % return_figures_dir()
-    fig.savefig(fname, dpi=100, bbox_inches='tight', pad_inches=0.01)
-    print('-- %s saved' % fname)
-    plt.close(fig)
+    # error hist
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    for i, key in enumerate(error.keys()):
+        arr = error[key]
+        bins = np.linspace(-2, 2, 50)
+        # ax[i].hist(arr, bins=bins, histtype='step', density=True)
+        ax[i].hist(arr[level5_cond], bins=bins, histtype='step', density=True, color='r')
+        ax[i].set_xlabel(f'error: {key}')
+    fig.tight_layout()
+
+    # param hist
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    lims = [(-2, 2), (1, 3), (0, 2), (-2, 2), (-2, 2), (0, 2)]
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    for i, key in enumerate(keys_err):
+        arr = data[key]
+        if key == 'tE':
+            bins = np.logspace(lims[i][0], lims[i][1], 50)
+            ax[i].set_xscale('log')
+        else:
+            bins = np.linspace(lims[i][0], lims[i][1], 50)
+        ax[i].hist(arr, bins=bins, histtype='step', density=True)
+        ax[i].hist(arr[level5_cond], bins=bins, histtype='step', density=True)
+        ax[i].set_xlabel(key)
+        if key == 'tE':
+            ax[i].axvline(150, color='k', alpha=.5)
+        elif key == 'piE':
+            ax[i].axvline(0.08, color='k', alpha=.5)
+    fig.tight_layout()
+
+    # param vs fractional error
+    fig, ax = plt.subplots(3, 2, figsize=(10, 10))
+    lims = [(-2, 2), (10, 500), (0, 2), (-2, 2), (-2, 2), (0, 2)]
+    ax = ax.flatten()
+    for a in ax: a.clear()
+    for i, key in enumerate(keys_err):
+        arr_data = data[key]
+        arr_error_frac = error_frac[key]
+        ax[i].scatter(arr_data, arr_error_frac, s=1, alpha=.1)
+        ax[i].scatter(arr_data[level5_cond], arr_error_frac[level5_cond], s=1, alpha=.1)
+        ax[i].set_xlim(lims[i])
+        ax[i].set_ylim(-2, 2)
+        ax[i].grid(True)
+        if key == 'tE':
+            ax[i].set_xscale('log')
+        ax[i].set_xlabel(key)
+        if key == 'tE':
+            ax[i].axvline(150, color='k', alpha=.5)
+        elif key == 'piE':
+            ax[i].axvline(0.08, color='k', alpha=.5)
+    fig.tight_layout()
 
 
 if __name__ == '__main__':
--- bin/pipeline/upload_level4_candidates_level5_cut.py
+++ bin/pipeline/upload_level4_candidates_level5_cut.py
@@ -4,7 +4,6 @@ upload_level4_candidates_level5_cut.py
 """
 
 import numpy as np
-import os
 
 from puzle.models import CandidateLevel4
 from puzle import db
@@ -42,14 +41,22 @@ def upload_level4_candidates_level5_cut():
         key_err = f'{key}_err'
         error_frac[key] = data[key_err] / data[key]
 
-    cond1 = error_frac['tE'] <= 0.5
-    cond2 = error_frac['b_sff'] <= 0.5
-    cond3 = error_frac['u0_amp'] <= 0.5
-    cond4 = error_frac['piE'] <= 0.5
-    cond5 = np.abs(data['u0_amp']) <= 1.0
-    cond6 = data['b_sff'] <= 1.2
-    cond7 = data['rchi2'] <= 5
-    level5_cond = cond1 * cond2 * cond3 * cond4 * cond5 * cond6 * cond7
+    cond1 = error_frac['tE'] <= 0.2
+    cond2 = data['u0_amp_err'] <= 0.1
+    cond3 = data['piE_err'] <= 0.1
+    cond4 = np.abs(data['u0_amp']) <= 1.0
+    cond5 = data['b_sff'] <= 1.2
+    cond6 = data['rchi2'] <= 3
+
+    print('No filters', len(cond1), 'cands')
+    print('Filters up to 1', np.sum(cond1), 'cands')
+    print('Filters up to 2', np.sum(cond1 * cond2), 'cands')
+    print('Filters up to 3', np.sum(cond1 * cond2 * cond3), 'cands')
+    print('Filters up to 4', np.sum(cond1 * cond2 * cond3 * cond4), 'cands')
+    print('Filters up to 5', np.sum(cond1 * cond2 * cond3 * cond4 * cond5), 'cands')
+    print('Filters up to 6', np.sum(cond1 * cond2 * cond3 * cond4 * cond5 * cond6), 'cands')
+
+    level5_cond = cond1 * cond2 * cond3 * cond4 * cond5 * cond6
 
     for i, (cand, level5_cond) in enumerate(zip(cands, level5_cond)):
         cand.level5 = level5_cond
