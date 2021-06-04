4e13156c58b716c80a0e59a7d2c6364192e07729
Michael Medford <MichaelMedford@berkeley.edu>
2021-06-04 08:47:00 -0400

    switch level5 definition to exclude u0_amp_err and piE_err

--- bin/pipeline/upload_level4_candidates_level5_cut.py
+++ bin/pipeline/upload_level4_candidates_level5_cut.py
@@ -5,13 +5,16 @@ upload_level4_candidates_level5_cut.py
 
 import numpy as np
 
-from puzle.models import CandidateLevel4
+from puzle.utils import MJD_finish
+from puzle.models import CandidateLevel3, CandidateLevel4
 from puzle import db
 
 
 def upload_level4_candidates_level5_cut():
-    cands = CandidateLevel4.query.\
-        filter(CandidateLevel4.pspl_gp_fit_finished == True,
+    cands = db.session.query(CandidateLevel4).outerjoin(CandidateLevel3,
+                                                        CandidateLevel4.id == CandidateLevel3.id). \
+        filter(CandidateLevel3.t0_best + CandidateLevel3.tE_best < MJD_finish,
+               CandidateLevel4.pspl_gp_fit_finished == True,
                CandidateLevel4.fit_type_pspl_gp != None).\
         all()
 
@@ -43,14 +46,12 @@ def upload_level4_candidates_level5_cut():
         error_frac[key] = data[key_err] / data[key]
 
     cond1 = error_frac['tE'] <= 0.2
-    cond2 = data['u0_amp_err'] <= 0.1
-    cond3 = data['piE_err'] <= 0.1
-    cond4 = np.abs(data['u0_amp']) <= 1.0
-    cond5 = data['b_sff'] <= 1.2
-    cond6 = data['rchi2'] <= 3
-    cond7 = data['delta_hmjd_outside'] / data['tE'] >= 2
+    cond2 = np.abs(data['u0_amp']) <= 1.0
+    cond3 = data['b_sff'] <= 1.2
+    cond4 = data['rchi2'] <= 3
+    cond5 = data['delta_hmjd_outside'] / data['tE'] >= 2
 
-    level5_cond = cond1 * cond2 * cond3 * cond4 * cond5 * cond6 * cond7
+    level5_cond = cond1 * cond2 * cond3 * cond4 * cond5
 
     print('No filters', len(cond1), 'cands')
     print('Filters up to 1', np.sum(cond1), 'cands')
@@ -58,8 +59,6 @@ def upload_level4_candidates_level5_cut():
     print('Filters up to 3', np.sum(cond1 * cond2 * cond3), 'cands')
     print('Filters up to 4', np.sum(cond1 * cond2 * cond3 * cond4), 'cands')
     print('Filters up to 5', np.sum(cond1 * cond2 * cond3 * cond4 * cond5), 'cands')
-    print('Filters up to 6', np.sum(cond1 * cond2 * cond3 * cond4 * cond5 * cond6), 'cands')
-    print('Filters up to 7', np.sum(cond1 * cond2 * cond3 * cond4 * cond5 * cond6 * cond7), 'cands')
 
     for i, (cand, level5_cond) in enumerate(zip(cands, level5_cond)):
         cand.level5 = level5_cond
