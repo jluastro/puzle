b693999443ea3cdb81d81c13daa59b6e2610336e
Michael Medford <MichaelMedford@berkeley.edu>
2021-06-05 16:50:48 -0400

    level5_cands_on_sky

--- bin/paper_figures/plot_job_cells.py
+++ bin/paper_figures/plot_job_cells.py
@@ -5,100 +5,16 @@ plot_job_cells.py
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-import os
-import pickle
 import numpy as np
 from scipy.stats import binned_statistic_2d
 from astropy.coordinates import SkyCoord
 
-from puzle.models import SourceIngestJob, StarProcessJob
-from puzle.utils import return_figures_dir, return_DR5_dir, return_data_dir
-from puzle import db
-
-
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i
-
-
-def _load_process_progress(type):
-    data_dir = return_data_dir()
-    fname = f'{data_dir}/{type}_process_progress.dict'
-    if os.path.exists(fname):
-        with open(fname, 'rb') as f:
-            return pickle.load(f)
-    else:
-        return {}
-
-
-def load_star_process_progress():
-    return _load_process_progress('star')
-
-
-def _save_process_progress(process_progress, type):
-    data_dir = return_data_dir()
-    fname = f'{data_dir}/{type}_process_progress.dict'
-    if os.path.exists(fname):
-        os.remove(fname)
-    with open(fname, 'wb') as f:
-        pickle.dump(process_progress, f)
-
-
-def save_star_process_progress(process_progress):
-    _save_process_progress(process_progress, 'star')
+from puzle.utils import return_figures_dir
+from puzle.jobs import return_num_objs_arr
 
 
 def plot_job_cells():
-    DR5_dir = return_DR5_dir()
-    jobs = db.session.query(SourceIngestJob, StarProcessJob).\
-        filter(SourceIngestJob.id == StarProcessJob.source_ingest_job_id).\
-        all()
-
-    star_process_progress = load_star_process_progress()
-
-    num_stars_arr = []
-    ra_arr = []
-    dec_arr = []
-    num_objs_arr = []
-    datetime_delta_arr = []
-    for i, job in enumerate(jobs):
-        if i % 1000 == 0:
-            print(i, len(jobs))
-        job_id = job[0].id
-        if job[1].num_objs is None:
-            num_objs = 0
-        else:
-            num_objs = job[1].num_objs
-
-        if job_id in star_process_progress:
-            ra = star_process_progress[job_id][0]
-            dec = star_process_progress[job_id][1]
-            num_stars = star_process_progress[job_id][2]
-        else:
-            ra = (job[0].ra_start + job[0].ra_end) / 2
-            dec = (job[0].dec_start + job[0].dec_end) / 2
-            dir = '%s/stars_%s' % (DR5_dir, str(job_id)[:3])
-            fname = f'{dir}/stars.{job_id:06}.txt'
-            num_stars = file_len(fname)
-            star_process_progress[job_id] = (ra, dec, num_stars)
-
-        datetime_delta = job[1].datetime_finished - job[1].datetime_started
-        datetime_delta_arr.append(datetime_delta.seconds)
-
-        num_stars_arr.append(num_stars)
-        num_objs_arr.append(num_objs)
-        ra_arr.append(ra)
-        dec_arr.append(dec)
-
-    save_star_process_progress(star_process_progress)
-
-    ra_arr = np.array(ra_arr)
-    dec_arr = np.array(dec_arr)
-    num_stars_arr = np.array(num_stars_arr)
-    num_objs_arr = np.array(num_objs_arr)
-    datetime_delta_arr = np.array(datetime_delta_arr)
+    ra_arr, dec_arr, num_objs_arr = return_num_objs_arr()
 
     ra_bins = np.arange(0, 361, 2)
     dec_bins = np.arange(-30, 91, 1)
--- bin/paper_figures/plot_level5_cands.py
+++ bin/paper_figures/plot_level5_cands.py
@@ -3,14 +3,18 @@
 plot_level5_cands.py
 """
 
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
+from matplotlib.patches import Circle, Rectangle
+from matplotlib.colors import LogNorm
 import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
 from collections import defaultdict
+from scipy.stats import binned_statistic_2d
 
 from puzle.models import CandidateLevel4
+from puzle.jobs import return_num_objs_arr
 from puzle.utils import return_figures_dir, return_data_dir
 
 
@@ -18,36 +22,77 @@ def plot_cands_on_sky():
     cands = CandidateLevel4.query.\
         filter(CandidateLevel4.level5 == True,
                CandidateLevel4.category != None).\
-        with_entities(CandidateLevel4.ra, CandidateLevel4.dec, CandidateLevel4.category).\
+        with_entities(CandidateLevel4.ra, CandidateLevel4.dec,
+                      CandidateLevel4.category).\
         all()
-    ra_arr = np.array([c.ra for c in cands])
-    dec_arr = np.array([c.dec for c in cands])
+    ra_cands_arr = np.array([c.ra for c in cands])
+    dec_cands_arr = np.array([c.dec for c in cands])
     category_arr = np.array([c.category for c in cands])
     clear_cond = category_arr == 'clear_microlensing'
-    coords_cands = SkyCoord(ra_arr, dec_arr, frame='icrs', unit='degree')
-    glon_arr = coords_cands.galactic.l.value
-    lb_cond = glon_arr >= 180
-    glon_arr[lb_cond] -= 360
-    glat_arr = coords_cands.galactic.b.value
+    coords_cands = SkyCoord(ra_cands_arr, dec_cands_arr, frame='icrs', unit='degree')
+    glon_cands_arr = coords_cands.galactic.l.value
+    lb_cond = glon_cands_arr >= 180
+    glon_cands_arr[lb_cond] -= 360
+    glat_cands_arr = coords_cands.galactic.b.value
 
     ra_ztf = np.linspace(0, 360, 10000)
-    dec_ztf = np.ones(len(ra_ztf)) * -20
+    dec_ztf = np.ones(len(ra_ztf)) * -30
     coords_ztf = SkyCoord(ra_ztf, dec_ztf, frame='icrs', unit='degree')
     glon_ztf = coords_ztf.galactic.l.value
     lb_cond = glon_ztf >= 180
     glon_ztf[lb_cond] -= 360
     glat_ztf = coords_ztf.galactic.b.value
 
-    fig, ax = plt.subplots(figsize=(8, 5))
-    ax.clear()
-    ax.scatter(glon_arr, glat_arr, c='b', s=5)
-    ax.scatter(glon_arr[clear_cond], glat_arr[clear_cond], c='r', s=5)
-    ax.set_xlabel('glon', fontsize=16)
-    ax.set_ylabel('glat', fontsize=16)
-    ax.scatter(glon_ztf, glat_ztf, color='k', s=1, alpha=.2)
-    ax.set_xlim(-180, 180)
-    ax.set_ylim(-90, 90)
+    ra_objs_arr, dec_objs_arr, num_objs_arr = return_num_objs_arr()
+    coords_objs = SkyCoord(ra_objs_arr, dec_objs_arr, frame='icrs', unit='degree')
+    glon_objs_arr = coords_objs.galactic.l.value
+    lb_cond = glon_objs_arr >= 180
+    glon_objs_arr[lb_cond] -= 360
+    glat_objs_arr = coords_objs.galactic.b.value
+    glon_bins = np.arange(-180, 181, 1.5)
+    glat_bins = np.arange(-90, 90, 1.5)
+    extent = (glon_bins.min(), glon_bins.max(), glat_bins.min(), glat_bins.max())
+    num_objs_hist = binned_statistic_2d(glon_objs_arr, glat_objs_arr, num_objs_arr,
+                                        statistic='sum', bins=(glon_bins, glat_bins))[0].T
+    num_objs_hist[num_objs_hist == 0] = 1
+
+    fig, ax = plt.subplots(2, 2, figsize=(14, 6))
+    ax = ax.flatten()
+    for a in ax: a.clear()
+
+    ax[0].scatter(glon_cands_arr, glat_cands_arr, c='b', s=3)
+    ax[0].set_xlim(-180, 180)
+    ax[0].set_ylim(-90, 90)
+
+    ax[1].scatter(glon_cands_arr[clear_cond], glat_cands_arr[clear_cond], c='r', s=3)
+    ax[1].set_xlim(-180, 180)
+    ax[1].set_ylim(-90, 90)
+
+    norm = LogNorm(vmin=1e4, vmax=8e5)
+    ax[2].imshow(num_objs_hist, norm=norm, extent=extent, origin='lower', aspect=0.75)
+
+    ax[3].scatter(glon_cands_arr[clear_cond], glat_cands_arr[clear_cond], c='r', s=3)
+    ax[3].set_xlim(0, 120)
+    ax[3].set_ylim(-30, 30)
+
+    for a in ax:
+        a.plot(glon_ztf, glat_ztf, color='k')
+        a.set_xlabel('glon', fontsize=16)
+        a.set_ylabel('glat', fontsize=16)
+        if a == ax[-1]:
+            continue
+        rect = Rectangle((0, -30), 120, 60, facecolor='none', edgecolor='k')
+        a.add_patch(rect)
+
     fig.tight_layout()
+
+    for xy in [(0, -30), (120, -30), (120, 30), (0, 30)]:
+        transFigure = fig.transFigure.inverted()
+        coord1 = transFigure.transform(ax[1].transData.transform([xy[0], xy[1]]))
+        coord2 = transFigure.transform(ax[3].transData.transform([xy[0], xy[1]]))
+        line = matplotlib.lines.Line2D((coord1[0], coord2[0]), (coord1[1], coord2[1]),
+                                       transform=fig.transFigure, color='k', alpha=.5, linewidth=.5)
+        fig.add_artist(line)
 
     popsycle_map_fname = return_data_dir() + '/popsycle_map.npz'
     popsycle_map = np.load(popsycle_map_fname)
@@ -56,9 +101,12 @@ def plot_cands_on_sky():
         b = popsycle_map['b'][i]
         area = popsycle_map['area'][i]
         radius = np.sqrt(area / np.pi)
-        patch = Circle((l, b), radius=radius+6,
-                       facecolor='g', edgecolor='None', alpha=0.2)
-        ax.add_patch(patch)
+        for a in ax:
+            if a == ax[2]:
+                continue
+            patch = Circle((l, b), radius=radius+6,
+                           facecolor='g', edgecolor='None', alpha=0.2)
+            a.add_patch(patch)
 
     fname = '%s/level5_cands_on_sky.png' % return_figures_dir()
     fig.savefig(fname, dpi=100, bbox_inches='tight', pad_inches=0.01)
@@ -66,20 +114,7 @@ def plot_cands_on_sky():
     plt.close(fig)
 
 
-def plot_cands_tE_overlapping_popsycle():
-    cands = CandidateLevel4.query.\
-        filter(CandidateLevel4.level5 == True,
-               CandidateLevel4.category == 'clear_microlensing').\
-        all()
-    cands = np.array(cands)
-    ra_arr = np.array([c.ra for c in cands])
-    dec_arr = np.array([c.dec for c in cands])
-    coords = SkyCoord(ra_arr, dec_arr, frame='icrs', unit='degree')
-    glon_arr = coords.galactic.l.value
-    cond = glon_arr >= 180
-    glon_arr[cond] -= 360
-    glat_arr = coords.galactic.b.value
-
+def fetch_popsycle_tE_piE(glon_arr, glat_arr):
     popsycle_map_fname = return_data_dir() + '/popsycle_map.npz'
     popsycle_map = np.load(popsycle_map_fname)
     glon_popsycle = popsycle_map['l']
@@ -97,8 +132,8 @@ def plot_cands_tE_overlapping_popsycle():
             cond_arr.append(False)
 
     popsycle_base_folder = '/global/cfs/cdirs/uLens/PopSyCLE_runs/PopSyCLE_runs_v3_refined_events'
-    tE_cands = [c.tE_pspl_gp for c in cands[cond_arr]]
     tE_popsycle = []
+    piE_popsycle = []
     for idx, num in popsycle_idx_dct.items():
         l = glon_popsycle[idx]
         b = glat_popsycle[idx]
@@ -108,18 +143,47 @@ def plot_cands_tE_overlapping_popsycle():
         popsycle_cond *= popsycle_catalog['delta_m_r'] >= 0.1
         popsycle_cond *= popsycle_catalog['f_blend_r'] >= 0.1
         popsycle_cond *= popsycle_catalog['ztf_r_app_LSN'] <= 22
+        num_popsycle_cond = np.sum(popsycle_cond)
+        if num_popsycle_cond == 0:
+            continue
+
         tE_popsycle_catalog = popsycle_catalog['t_E'][popsycle_cond]
         tE_popsycle_sample = np.random.choice(tE_popsycle_catalog, size=num, replace=True)
         tE_popsycle.extend(list(tE_popsycle_sample))
 
+        piE_popsycle_catalog = popsycle_catalog['pi_E'][popsycle_cond]
+        piE_popsycle_sample = np.random.choice(piE_popsycle_catalog, size=num, replace=True)
+        piE_popsycle.extend(list(piE_popsycle_sample))
+
+    return cond_arr, tE_popsycle, piE_popsycle
+
+
+def plot_cands_tE_overlapping_popsycle():
+    cands = CandidateLevel4.query.\
+        filter(CandidateLevel4.level5 == True,
+               CandidateLevel4.category == 'clear_microlensing').\
+        all()
+    cands = np.array(cands)
+    ra_arr = np.array([c.ra for c in cands])
+    dec_arr = np.array([c.dec for c in cands])
+    coords = SkyCoord(ra_arr, dec_arr, frame='icrs', unit='degree')
+    glon_arr = coords.galactic.l.value
+    cond = glon_arr >= 180
+    glon_arr[cond] -= 360
+    glat_arr = coords.galactic.b.value
+
+    cond_arr, tE_popsycle, _ = fetch_popsycle_tE_piE(glon_arr, glat_arr)
+    tE_cands = np.array([c.tE_pspl_gp for c in cands[cond_arr]])
+
     fig, ax = plt.subplots(figsize=(8, 5))
-    bins = np.logspace(0, 3, 20)
+    bins = np.logspace(0.5, 3, 20)
     ax.clear()
-    ax.hist(tE_cands, bins=bins, histtype='step', color='r')
-    ax.hist(tE_popsycle, bins=bins, histtype='step', color='b')
+    ax.hist(tE_cands, bins=bins, histtype='step', color='r', label='candidates')
+    ax.hist(tE_popsycle, bins=bins, histtype='step', color='b', label='popsycle')
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlabel(r'$t_E$', fontsize=16)
+    ax.legend()
     fig.tight_layout()
 
     fname = '%s/level5_cands_tE.png' % return_figures_dir()
@@ -142,21 +206,7 @@ def plot_cands_tE_piE_overlapping_popsycle():
     glon_arr[cond] -= 360
     glat_arr = coords.galactic.b.value
 
-    popsycle_map_fname = return_data_dir() + '/popsycle_map.npz'
-    popsycle_map = np.load(popsycle_map_fname)
-    glon_popsycle = popsycle_map['l']
-    glat_popsycle = popsycle_map['b']
-
-    popsycle_idx_dct = defaultdict(int)
-    cond_arr = []
-    for glon, glat in zip(glon_arr, glat_arr):
-        dist = np.hypot(np.cos(np.radians(glat)) * (glon - glon_popsycle),
-                        glat - glat_popsycle)
-        if np.min(dist) < 6:
-            popsycle_idx_dct[np.argmin(dist)] += 1
-            cond_arr.append(True)
-        else:
-            cond_arr.append(False)
+    cond_arr, tE_popsycle, piE_popsycle = fetch_popsycle_tE_piE(glon_arr, glat_arr)
 
     tE_cands = np.array([c.tE_pspl_gp for c in cands[cond_arr]])
     tE_err_cands = np.array([c.tE_err_pspl_gp for c in cands[cond_arr]])
@@ -164,42 +214,22 @@ def plot_cands_tE_piE_overlapping_popsycle():
     piE_err_cands = np.array([c.piE_err_pspl_gp for c in cands[cond_arr]])
     cond_piE_err = piE_err_cands <= 0.1
 
-    popsycle_base_folder = '/global/cfs/cdirs/uLens/PopSyCLE_runs/PopSyCLE_runs_v3_refined_events'
-    tE_popsycle = []
-    piE_popsycle = []
-    for idx, num in popsycle_idx_dct.items():
-        l = glon_popsycle[idx]
-        b = glat_popsycle[idx]
-        popsycle_fname = f'{popsycle_base_folder}/l{l:.1f}_b{b:.1f}_refined_events_ztf_r_Damineli16.fits'
-        popsycle_catalog = Table.read(popsycle_fname, format='fits')
-        popsycle_cond = popsycle_catalog['u0'] <= 1.0
-        popsycle_cond *= popsycle_catalog['delta_m_r'] >= 0.1
-        popsycle_cond *= popsycle_catalog['f_blend_r'] >= 0.1
-        popsycle_cond *= popsycle_catalog['ztf_r_app_LSN'] <= 22
-
-        tE_popsycle_catalog = popsycle_catalog['t_E'][popsycle_cond]
-        tE_popsycle_sample = np.random.choice(tE_popsycle_catalog, size=num, replace=True)
-        tE_popsycle.extend(list(tE_popsycle_sample))
-
-        piE_popsycle_catalog = popsycle_catalog['pi_E'][popsycle_cond]
-        piE_popsycle_sample = np.random.choice(piE_popsycle_catalog, size=num, replace=True)
-        piE_popsycle.extend(list(piE_popsycle_sample))
-
     fig, ax = plt.subplots(figsize=(8, 5))
     ax.clear()
-    ax.scatter(tE_popsycle, piE_popsycle, color='b')
-    ax.errorbar(tE_cands, piE_cands,
-                xerr=tE_err_cands, yerr=piE_err_cands,
+    ax.scatter(tE_popsycle, piE_popsycle, color='b', s=5, label='popsycle')
+    ax.errorbar(tE_cands[~cond_piE_err], piE_cands[~cond_piE_err],
+                xerr=tE_err_cands[~cond_piE_err], yerr=piE_err_cands[~cond_piE_err],
                 color='r', linestyle='', alpha=.2)
-    ax.scatter(tE_cands, piE_cands, color='r')
+    ax.scatter(tE_cands[~cond_piE_err], piE_cands[~cond_piE_err], color='r', s=5, label='level5')
     ax.errorbar(tE_cands[cond_piE_err], piE_cands[cond_piE_err],
                 xerr=tE_err_cands[cond_piE_err], yerr=piE_err_cands[cond_piE_err],
                 color='g', linestyle='', alpha=.2)
-    ax.scatter(tE_cands[cond_piE_err], piE_cands[cond_piE_err], color='g')
+    ax.scatter(tE_cands[cond_piE_err], piE_cands[cond_piE_err], color='g', s=5, label='level5 cut on u0_err, piE_err')
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlabel(r'$t_E$', fontsize=16)
     ax.set_ylabel(r'$\pi_E$', fontsize=16)
+    ax.legend(markerscale=3)
     fig.tight_layout()
 
     fname = '%s/level5_cands_tE_piE.png' % return_figures_dir()
